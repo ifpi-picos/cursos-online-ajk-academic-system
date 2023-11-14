1a7582f68588ff27c4e2993bca4ef51e73e5113b
karielly <kariellyycarvalho@gmail.com>
2023-11-13 23:45:06 -0300

    feat(entities): criar atributos

---
+++ src/main/java/br/edu/ifpi/dao/TeacherDao.java
@@ -0,0 +1,5 @@
+package br.edu.ifpi.dao;
+
+public class TeacherDao {
+    
+}
--- src/main/java/br/edu/ifpi/entities/Teacher.java
+++ src/main/java/br/edu/ifpi/entities/Teacher.java
@@ -1,5 +1,33 @@
 package br.edu.ifpi.entities;
 
 public class Teacher {
+    private String name;
+    private String id;
+    private String email;
     
+    public Teacher(String name, String id, String email){
+        this.name = name;
+        this.id = id;
+        this.email = email;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public String getId(){
+        return id;
+    }
+
+    public String getEmail(){
+        return email;
+    }
+
+    public void setEmail(String email){
+        this.email = email;
+    }
 }
