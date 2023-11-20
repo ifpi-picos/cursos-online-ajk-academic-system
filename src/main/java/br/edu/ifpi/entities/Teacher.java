b755aec6c3bf771240f7c4725dbcabcde27bc065
karielly <kariellyycarvalho@gmail.com>
2023-11-20 03:25:02 -0300

    feat(Teacher): add novo atributo

--- src/main/java/br/edu/ifpi/entities/Teacher.java
+++ src/main/java/br/edu/ifpi/entities/Teacher.java
@@ -4,16 +4,19 @@ public class Teacher {
     private String name;
     private int id;
     private String email;
+    private String password;
 
-    public Teacher(int id, String name, String email) {
+    public Teacher(int id, String name, String email, String password) {
         this.id = id;
         this.name = name;
         this.email = email;
+        this.password = password;
     }
 
-    public Teacher(String name, String email) {
+    public Teacher(String name, String email, String password) {
         this.name = name;
         this.email = email;
+        this.password = password;
     }
 
     public String getName() {
@@ -34,5 +37,13 @@ public class Teacher {
 
     public void setEmail(String email) {
         this.email = email;
+    }
+
+    public String getPassword() { 
+        return password; 
+    }
+
+    public void setPassword(String password) { 
+        this.password = password; 
     }
 }
