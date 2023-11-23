66aac438cb0ba1cfd56792bd82a019a50337bb14
karielly <kariellyycarvalho@gmail.com>
2023-11-22 22:53:50 -0300

    Add tableRegister class for student controller

---
+++ src/main/java/br/edu/ifpi/controllers/student/tableRegister.java
@@ -0,0 +1,59 @@
+package br.edu.ifpi.controllers.student;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.text.Text;
+
+public class tableRegister {
+
+    @FXML
+    private Button exit;
+
+    @FXML
+    private Button registerCourse;
+
+    @FXML
+    private TableView<?> tableRegister;
+
+    @FXML
+    private Text username;
+
+    @FXML
+    void completedCourses(ActionEvent event) {
+
+    }
+
+    @FXML
+    void exit(ActionEvent event) {
+
+    }
+
+    @FXML
+    void home(ActionEvent event) {
+
+    }
+
+    @FXML
+    void ongoingCourses(ActionEvent event) {
+
+    }
+
+    @FXML
+    void register(ActionEvent event) {
+
+    }
+
+    @FXML
+    void registerCourse(ActionEvent event) {
+
+    }
+
+    @FXML
+    void usernameButton(MouseEvent event) {
+
+    }
+
+}
