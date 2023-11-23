d35fb58d51acbbf999b2127e65eefc7a582a9bed
karielly <kariellyycarvalho@gmail.com>
2023-11-23 12:34:47 -0300

    Fix final_grade column in Student_course table

--- src/main/java/br/edu/ifpi/data/dao/DatabaseConnection.java
+++ src/main/java/br/edu/ifpi/data/dao/DatabaseConnection.java
@@ -57,7 +57,7 @@ public class DatabaseConnection {
             statement.executeUpdate("CREATE TABLE IF NOT EXISTS Student_course ("
                     + "student_id INT NOT NULL,"
                     + "course_id INT NOT NULL,"
-                    + "final_grade DECIMAL(4, 2) NOT NULL,"
+                    + "final_grade DECIMAL(4, 2),"
                     + "status VARCHAR(255) NOT NULL, "
                     + "PRIMARY KEY (student_id, course_id),"
                     + "FOREIGN KEY (student_id) REFERENCES Student(id),"
