f90483c4031aae9c2f9647d21de94cb9cd87f02b
Jean Carlos R. Sousa <jeancarllos99@outlook.com>
2023-11-23 12:27:28 -0300

    Remove unused imports in LoginController

--- src/main/java/br/edu/ifpi/controllers/LoginController.java
+++ src/main/java/br/edu/ifpi/controllers/LoginController.java
@@ -2,8 +2,6 @@ package br.edu.ifpi.controllers;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Alert;
-import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
