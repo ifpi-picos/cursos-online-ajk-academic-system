f9488dcdb9c90638d9ff82ebacde54ca47d69802
karielly <kariellyycarvalho@gmail.com>
2023-11-22 22:54:01 -0300

    Add course selection functionality and display alert if no course is selected

--- src/main/java/br/edu/ifpi/controllers/student/RegisterCourseController.java
+++ src/main/java/br/edu/ifpi/controllers/student/RegisterCourseController.java
@@ -11,11 +11,13 @@ import br.edu.ifpi.entities.Student;
 import br.edu.ifpi.util.SceneNavigator;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.stage.Stage;
 
 import javafx.scene.control.TableView;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.text.Text;
@@ -60,6 +62,16 @@ public class RegisterCourseController extends StudentHomeController {
 
     @FXML
     void registerCourse(ActionEvent event) {
+        Course selectedItem = tableRegister.getSelectionModel().getSelectedItem();
+        if (selectedItem != null) {
+            System.out.println("Curso selecionado: " + selectedItem.getName());
+        } else {
+            Alert alert = new Alert(AlertType.WARNING);
+            alert.setTitle("Aviso");
+            alert.setHeaderText("Nenhum curso selecionado");
+            alert.setContentText("Selecione um curso para se matricular");
+            alert.showAndWait();
+        }
 
     }
 
@@ -71,21 +83,30 @@ public class RegisterCourseController extends StudentHomeController {
     public void initialize(URL location, ResourceBundle resources) {
         loadTableCourse();
         tableRegister.setItems(observableListCourse);
+
+        tableRegister.getSelectionModel().selectedItemProperty()
+                .addListener((observable, oldValue, newValue) -> selectCourseTableItem(newValue));
+
     }
 
     public void loadTableCourse() {
 
-        // Course c1 = new Course(1, "Informática", null, 40, 1);
-        // Course c2 = new Course(2, "Informática", null, 40, 1);
+        Course c1 = new Course(1, "Informática", null, 40, 1);
+        Course c2 = new Course(2, "Informática", null, 40, 1);
 
-        // courses.add(c1);
-        // courses.add(c2);
+        courses.add(c1);
+        courses.add(c2);
 
         id.setCellValueFactory(new PropertyValueFactory<>("id"));
         name.setCellValueFactory(new PropertyValueFactory<>("name"));
         teacher_id.setCellValueFactory(new PropertyValueFactory<>("teacherId"));
         workload.setCellValueFactory(new PropertyValueFactory<>("workload"));
         observableListCourse = FXCollections.observableArrayList(courses);
+    }
+
+    public void selectCourseTableItem(Course course) {
+        System.out.println("Curso selecionado: " + course.getName());
+
     }
 
 }
