78d97d0ec189e33bea352d4dcb3f69a7f79b635b
Jean Carlos R. Sousa <jeancarllos99@outlook.com>
2023-11-23 23:24:10 -0300

    refactor(TeacherDao): refatorando o código do DAO

--- src/main/java/br/edu/ifpi/data/dao/TeacherDao.java
+++ src/main/java/br/edu/ifpi/data/dao/TeacherDao.java
@@ -18,11 +18,29 @@ public class TeacherDao implements Dao<Teacher> {
         this.connection = connection;
     }
 
+    public Teacher login(String username, String password) {
+        final String sql = "SELECT * FROM Teacher WHERE (email = ? OR name = ?) AND password = ?";
+        try (PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+            preparedStatement.setString(1, username);
+            preparedStatement.setString(2, username);
+            preparedStatement.setString(3, password);
+            final ResultSet resultSet = preparedStatement.executeQuery();
+            if (resultSet.next()) {
+                return TeacherAdapter.fromResultSet(resultSet);
+            }
+        } catch (SQLException e) {
+            System.err.format("SQL State: %s\n%s", e.getSQLState(), e.getMessage());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
     @Override
     public int insert(Teacher teacher) {
         final String sql = "INSERT INTO Teacher (name, email, password) VALUES (?, ?, ?)";
-        try {
-            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+        try (PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
             preparedStatement.setString(1, teacher.getName());
             preparedStatement.setString(2, teacher.getEmail());
             preparedStatement.setString(3, teacher.getPassword());
@@ -38,8 +56,7 @@ public class TeacherDao implements Dao<Teacher> {
     @Override
     public int update(Teacher teacher) {
         final String sql = "UPDATE Teacher SET name = ?, email = ?, password = ? WHERE id = ?";
-        try {
-            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+        try (PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
             preparedStatement.setString(1, teacher.getName());
             preparedStatement.setString(2, teacher.getEmail());
             preparedStatement.setString(3, teacher.getPassword());
@@ -56,8 +73,7 @@ public class TeacherDao implements Dao<Teacher> {
     @Override
     public int delete(int id) {
         final String sql = "DELETE FROM Teacher WHERE id = ?";
-        try {
-            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+        try (PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
             preparedStatement.setInt(1, id);
             return preparedStatement.executeUpdate();
         } catch (SQLException e) {
@@ -71,8 +87,7 @@ public class TeacherDao implements Dao<Teacher> {
     @Override
     public Teacher select(int id) {
         final String sql = "SELECT * FROM Teacher WHERE id = ?";
-        try {
-            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+        try (PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
             preparedStatement.setInt(1, id);
             final ResultSet resultSet = preparedStatement.executeQuery();
             if (resultSet.next()) {
@@ -90,8 +105,7 @@ public class TeacherDao implements Dao<Teacher> {
     public List<Teacher> selectAll() {
         final String sql = "SELECT * FROM Teacher";
         List<Teacher> teachers = new ArrayList<>();
-        try {
-            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+        try (PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
             final ResultSet resultSet = preparedStatement.executeQuery();
 
             while (resultSet.next()) {
@@ -110,8 +124,7 @@ public class TeacherDao implements Dao<Teacher> {
     public List<Teacher> selectAll(String condition) {
         final String sql = "SELECT * FROM Teacher WHERE " + condition;
         List<Teacher> teachers = new ArrayList<>();
-        try {
-            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+        try (PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
             final ResultSet resultSet = preparedStatement.executeQuery();
 
             while (resultSet.next()) {
@@ -130,8 +143,7 @@ public class TeacherDao implements Dao<Teacher> {
     public List<Teacher> selectAll(String[] conditions) {
         final String sql = "SELECT * FROM Teacher WHERE " + String.join(" AND ", conditions);
         List<Teacher> teachers = new ArrayList<>();
-        try {
-            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+        try (PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
             final ResultSet resultSet = preparedStatement.executeQuery();
 
             while (resultSet.next()) {
