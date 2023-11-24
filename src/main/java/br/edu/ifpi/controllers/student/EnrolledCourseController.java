641cff8f6908063046efc733d4f07b2f5faac3da
karielly <kariellyycarvalho@gmail.com>
2023-11-24 01:17:41 -0300

    Refactor EnrolledCourseController to display enrolled courses and allow cancellation

--- src/main/java/br/edu/ifpi/controllers/student/EnrolledCourseController.java
+++ src/main/java/br/edu/ifpi/controllers/student/EnrolledCourseController.java
@@ -1,34 +1,43 @@
 package br.edu.ifpi.controllers.student;
 
+import java.net.URL;
 import java.sql.Connection;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.ResourceBundle;
 
-import br.edu.ifpi.data.dao.CourseDao;
+import br.edu.ifpi.config.Routes;
 import br.edu.ifpi.data.dao.StudentCourseDao;
 import br.edu.ifpi.entities.Course;
 import br.edu.ifpi.entities.Student;
 import br.edu.ifpi.entities.StudentCourse;
+import br.edu.ifpi.entities.enums.EnrollmentStatus;
+import br.edu.ifpi.util.AlertMessage;
 import br.edu.ifpi.util.SceneNavigator;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
-import javafx.scene.text.Text;
+import javafx.scene.control.TableView;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
 
 public class EnrolledCourseController extends StudentHomeController {
 
-    private final CourseDao coursesDao;
     private final StudentCourseDao studentCourseDao;
     private ObservableList<Course> observableListCourse;
 
     public EnrolledCourseController(Connection connection, SceneNavigator sceneNavigator, Student student,
             Stage stage) {
         super(connection, sceneNavigator, student, stage);
-        this.coursesDao = new CourseDao(connection);
         this.studentCourseDao = new StudentCourseDao(connection);
     }
+
+    @FXML
+    private TableView<Course> tableEnrolledCourses;
 
     @FXML
     private TableColumn<Course, Integer> id;
@@ -44,15 +53,50 @@ public class EnrolledCourseController extends StudentHomeController {
 
     @FXML
     void cancelRegistration(ActionEvent event) {
+        Course course = tableEnrolledCourses.getSelectionModel().getSelectedItem();
+        StudentCourse studentCourse = studentCourseDao.select(student.getId(), course.getId());
+        studentCourse.setEnrollmentStatus(EnrollmentStatus.CANCELED);
 
+        int row = studentCourseDao.update(studentCourse);
+        if (row > 0) {
+            AlertMessage.show("Sucesso", "Sucesso", "Matrícula cancelada com sucesso", AlertType.INFORMATION);
+
+            sceneNavigator.navigateTo(Routes.enrolledCourse, this.stage, this);
+        } else {
+            AlertMessage.show("Erro", "Erro", "Erro ao cancelar matrícula", AlertType.ERROR);
+        }
     }
 
-    // mostrar os cursos que o aluno está matriculado e que ainda não foram
-    // concluídos
     @FXML
-    public void loadEnrolledStudents() {
-        List<StudentCourse> studentCourses = studentCourseDao.selectAll("student_id = " + student.getId());
+    public void loadEnrolledCourses() {
+        String condition = "student_id = " + student.getId() + " AND status = '" + EnrollmentStatus.PENDING + "'";
+        List<StudentCourse> studentCourses = studentCourseDao.selectAll(condition);
+        List<Course> courses = new ArrayList<>();
+
+        for (StudentCourse studentCourse : studentCourses) {
+            Course course = studentCourse.getCourse();
+            courses.add(course);
+        }
+
+        id.setCellValueFactory(new PropertyValueFactory<>("id"));
+        name.setCellValueFactory(new PropertyValueFactory<>("name"));
+        teacher.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue().getTeacher().getName()));
+        workload.setCellValueFactory(new PropertyValueFactory<>("workload"));
+        observableListCourse = FXCollections.observableArrayList(courses);
+    }
+
+    public void selectCourseTableItem(Course course) {
+        System.out.println("Curso selecionado: " + course.getName());
 
     }
 
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        loadEnrolledCourses();
+
+        tableEnrolledCourses.setItems(observableListCourse);
+        tableEnrolledCourses.getSelectionModel().selectedItemProperty().addListener(
+                (observable, oldValue, newValue) -> selectCourseTableItem(newValue));
+
+    }
 }
