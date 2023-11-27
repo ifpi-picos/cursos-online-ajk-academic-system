3e8747cd016aaee7d8595cce743850c00c9cf51e
Jean Carlos R. Sousa <jeancarllos99@outlook.com>
2023-11-27 01:52:43 -0300

    Refactor TeacherCourseController and TeacherOpenCourseController

--- src/main/java/br/edu/ifpi/controllers/teacher/TeacherCourseController.java
+++ src/main/java/br/edu/ifpi/controllers/teacher/TeacherCourseController.java
@@ -2,28 +2,39 @@ package br.edu.ifpi.controllers.teacher;
 
 import java.net.URL;
 import java.sql.Connection;
+import java.util.List;
 import java.util.ResourceBundle;
 
+import br.edu.ifpi.config.Routes;
 import br.edu.ifpi.data.dao.CourseDao;
+import br.edu.ifpi.data.dao.StudentCourseDao;
 import br.edu.ifpi.entities.Course;
+import br.edu.ifpi.entities.StudentCourse;
 import br.edu.ifpi.entities.Teacher;
+import br.edu.ifpi.entities.enums.CourseStatus;
 import br.edu.ifpi.util.SceneNavigator;
-
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class TeacherCourseController extends TeacherHomeController {
 
     private ObservableList<Course> observableListCourse;
+    private final CourseDao courseDao;
+    private final StudentCourseDao studentCourseDao;
 
     public TeacherCourseController(Connection connection, SceneNavigator sceneNavigator, Teacher teacher, Stage stage) {
         super(connection, sceneNavigator, teacher, stage);
-        new CourseDao(connection);
+        courseDao = new CourseDao(connection);
+        studentCourseDao = new StudentCourseDao(connection);
     }
 
     @FXML
@@ -33,26 +44,62 @@ public class TeacherCourseController extends TeacherHomeController {
     private TableColumn<Course, String> status;
 
     @FXML
-    private TableView<Course> tableCourse;
+    private TableColumn<Course, Integer> workload;
+
+    @FXML
+    private TableView<Course> tableCourses;
+
+    @FXML
+    private TableColumn<Course, Integer> id;
+
+    @FXML
+    private TableColumn<Course, Integer> numberStudents;
 
     @FXML
     private Text username;
 
+    public void openCourse(ActionEvent event) {
+        Course course = tableCourses.getSelectionModel().getSelectedItem();
+        TeacherOpenCourseController teacherOpenCourseController = new TeacherOpenCourseController(
+                connection,
+                sceneNavigator,
+                teacher,
+                stage,
+                course);
+
+        sceneNavigator.navigateTo(Routes.teacherOpenCourse, this.stage, teacherOpenCourseController);
+    }
+
+    public void loadTableCourse() {
+        List<Course> courses = courseDao.selectAll();
+        List<StudentCourse> studentCourses = studentCourseDao.selectAll();
+
+        id.setCellValueFactory(new PropertyValueFactory<>("id"));
+        name.setCellValueFactory(new PropertyValueFactory<>("name"));
+        workload.setCellValueFactory(new PropertyValueFactory<>("workload"));
+        numberStudents.setCellValueFactory(cellData -> new SimpleIntegerProperty(studentsByCourse(cellData.getValue(), studentCourses)).asObject());
+
+        // ordenar por id
+        courses.sort((c1, c2) -> c1.getId() - c2.getId());
+
+        observableListCourse = FXCollections.observableArrayList(courses);
+    }
+
+    public int studentsByCourse(Course course, List<StudentCourse> studentCourses) {
+        int count = 0;
+        for (StudentCourse studentCourse : studentCourses) {
+            if (studentCourse.getCourse().getId() == course.getId()) {
+                count++;
+            }
+        }
+        return count;
+    }
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         username.setText("Olá, " + teacher.getName());
-        
-        if (tableCourse != null) {
-            tableCourse.setItems(observableListCourse);
-        } else {
-            System.out.println("tableCourse é nulo!");
-        }
-    }
 
-    public void openCourse(ActionEvent event) {
-        
-    }
-
-    public void seeStudents(ActionEvent event) {
+        loadTableCourse();
+        tableCourses.setItems(observableListCourse);
     }
 }
--- src/main/java/br/edu/ifpi/controllers/teacher/TeacherOpenCourseController.java
+++ src/main/java/br/edu/ifpi/controllers/teacher/TeacherOpenCourseController.java
@@ -2,13 +2,18 @@ package br.edu.ifpi.controllers.teacher;
 
 import java.net.URL;
 import java.sql.Connection;
+import java.util.List;
 import java.util.ResourceBundle;
 
 import br.edu.ifpi.data.dao.CourseDao;
+import br.edu.ifpi.data.dao.StudentCourseDao;
 import br.edu.ifpi.entities.Course;
+import br.edu.ifpi.entities.StudentCourse;
 import br.edu.ifpi.entities.Teacher;
+import br.edu.ifpi.entities.enums.EnrollmentStatus;
 import br.edu.ifpi.util.SceneNavigator;
-
+import javafx.beans.property.SimpleStringProperty;
+import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -16,43 +21,91 @@ import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
+import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
 
 public class TeacherOpenCourseController extends TeacherHomeController {
 
-    private ObservableList<Course> observableListCourse;
+    private ObservableList<StudentCourse> observableListStudentCourse;
+    private final StudentCourseDao studentCourseDao;
+    private final Course course;
 
-    public TeacherOpenCourseController(Connection connection, SceneNavigator sceneNavigator, Teacher teacher, Stage stage) {
+    public TeacherOpenCourseController(Connection connection, SceneNavigator sceneNavigator, Teacher teacher,
+            Stage stage, Course course) {
+
         super(connection, sceneNavigator, teacher, stage);
-        new CourseDao(connection);
+        studentCourseDao = new StudentCourseDao(connection);
+        this.course = course;
+
     }
 
     @FXML
-    private TableColumn<Course, String> name;
+    private TableColumn<StudentCourse, String> email;
 
     @FXML
-    private TableColumn<Course, String> status;
+    private TableColumn<StudentCourse, Double> grade;
 
     @FXML
-    private TableView<Course> tableCourse;
+    private TextField gradeValue;
+
+    @FXML
+    private TableColumn<StudentCourse, Integer> id;
+
+    @FXML
+    private TableColumn<StudentCourse, String> name;
+
+    @FXML
+    private Text nameCourse;
+
+    @FXML
+    private TableColumn<StudentCourse, String> status;
+
+    @FXML
+    private TableView<StudentCourse> tableCourse;
 
     @FXML
     private Text username;
 
-    @Override
-    public void initialize(URL location, ResourceBundle resources) {
-        username.setText("Olá, " + teacher.getName());
-        
-        if (tableCourse != null) {
-            tableCourse.setItems(observableListCourse);
+    @FXML
+    void backCourses(ActionEvent event) {
+
+    }
+
+    @FXML
+    void giveGrade(ActionEvent event) {
+
+    }
+
+    public void loadTableCourse() {
+        List<StudentCourse> studentCourses = studentCourseDao.selectAll("course_id = " + course.getId());
+
+        id.setCellValueFactory(new PropertyValueFactory<>("id"));
+        name.setCellValueFactory(new PropertyValueFactory<>("name"));
+        email.setCellValueFactory(new PropertyValueFactory<>("email"));
+        grade.setCellValueFactory(new PropertyValueFactory<>("finalGrade"));
+        status.setCellValueFactory(cellData -> new SimpleStringProperty(getStatus(cellData.getValue())));
+
+        observableListStudentCourse = FXCollections.observableArrayList(studentCourses);
+    }
+
+    public String getStatus(StudentCourse studentCourse) {
+        if (studentCourse.getEnrollmentStatus() == EnrollmentStatus.PENDING) {
+            return "Pendente";
+        } else if (studentCourse.getEnrollmentStatus() == EnrollmentStatus.APPROVED) {
+            return "Aprovado";
+        } else if (studentCourse.getEnrollmentStatus() == EnrollmentStatus.CANCELED) {
+            return "Cancelado";
+        } else if (studentCourse.getEnrollmentStatus() == EnrollmentStatus.REPROVED) {
+            return "Reprovado";
         } else {
-            System.out.println("tableCourse é nulo!");
+            return "Sem status";
         }
     }
 
-    public void openCourse(ActionEvent event) {
-        
-    }
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        username.setText("Olá, " + teacher.getName());
+        nameCourse.setText(this.course.getName());
 
-    public void seeStudents(ActionEvent event) {
     }
 }
